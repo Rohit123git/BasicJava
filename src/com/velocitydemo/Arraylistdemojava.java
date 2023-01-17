8d261e308b745407a71f5fcd70f04b621f7388b7
rohit <rohitpathare41@gmail.com>
2023-01-18 01:01:49 +0530

    Added Arralist class

---
+++ src/com/velocitydemo/Arraylistdemojava.java
@@ -0,0 +1,22 @@
+
+		package com.velocitydemo;
+
+		import java.util.ArrayList;
+		import java.util.List;
+
+		public class Arraylistdemojava {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+
+			List<String> list = new ArrayList<String>();
+			list.add("java");
+			list.add("Hibernate");
+			System.out.println(list);
+			}
+
+	
+
+	}
+
+
